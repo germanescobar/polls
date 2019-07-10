65612ed13a28fc9e28f3855e0f9fe4e239c3e31b
dcmarin91 <49970223+dcmarin91@users.noreply.github.com>
2019-07-10 08:47:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,37 +12,7 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true}));
 
 
-//Crear documentos BD
 
-/*
-const user = new User({
-    email: 'pedro4@gmail.com',
-    name: "Pedro4"
-});
-user.save(function (err) {
-    if (err) return console.log(err);
-});
-const poll = new Poll({
-  title: 'Encuesta 4',
-  description: "Ejemplo 4",
-  user: user._id,    // assign the _id from the person
-  options: [{
-    text: "Opción 1"
-  }, {
-    text: "Opción 2"
-  }, {
-    text: "Opción 3"
-  }, {
-    text: "Opción 4"
-  }]
-});
-poll.save(function (err, res) {
-    if (err) return console.log(err);
-    console.log("Poll created");
-    // thats it!
-});
-*/
-// listar todas las encuestas
 app.get("/", async (req, res, next) => {
     const polls = await Poll.find().populate('user');
     res.render("index",{ polls });
