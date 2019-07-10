484708e25f65e35623544b8e4254de8b835f3799
dcmarin91 <dcmarin91@hotmail.com>
2019-07-10 08:34:35 -0500

    Se agrega Listar encuenstas

--- index.js
+++ index.js
@@ -9,10 +9,43 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
+app.use(express.urlencoded({ extended: true}));
 
+
+//Crear documentos BD
+
+/*
+const user = new User({
+    email: 'pedro4@gmail.com',
+    name: "Pedro4"
+});
+user.save(function (err) {
+    if (err) return console.log(err);
+});
+const poll = new Poll({
+  title: 'Encuesta 4',
+  description: "Ejemplo 4",
+  user: user._id,    // assign the _id from the person
+  options: [{
+    text: "Opción 1"
+  }, {
+    text: "Opción 2"
+  }, {
+    text: "Opción 3"
+  }, {
+    text: "Opción 4"
+  }]
+});
+poll.save(function (err, res) {
+    if (err) return console.log(err);
+    console.log("Poll created");
+    // thats it!
+});
+*/
 // listar todas las encuestas
 app.get("/", async (req, res, next) => {
-
+    const polls = await Poll.find().populate('user');
+    res.render("index",{ polls });
 });
 
 // formulario para crear una encuesta
@@ -37,7 +70,7 @@ app.get("/polls/:id", (req, res) => {
 
 // votar por una encuesta
 app.post("/polls/:id/vote", (req, res) => {
-
+  //const vote = await Poll.findByIdAndUpdate()
 });
 
 // ver los resultados de una encuesta
