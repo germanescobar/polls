f18fbef4840ac29e7aae3d0e0fde17c67ff3b366
Mauricio <maudob@live.com>
2019-07-11 00:16:16 -0500

    adding register and login forms

--- index.js
+++ index.js
@@ -2,20 +2,90 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const Poll = require("./models/Poll");
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 
 mongoose.connect("mongodb://127.0.0.1:27017/polls_top", { useNewUrlParser: true });
 
 const app = express();
-
+app.use(cookieSession({
+  secret: 'hola',
+  maxAge:  10* 60 * 1000,
+}));
 app.set('view engine', 'pug');
 app.set('views', 'views');
-
 app.use(express.urlencoded({ extended: true}));
 
 app.get("/", async (req, res, next) => {
   const polls = await Poll.find().populate('user');
   res.render("index", { polls });
 });
+
+// Formulario de registro
+app.get('/register', async (req, res) => {
+  try {
+    res.render('register');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Registrar un nuevo usuario
+app.post('/register', async (req, res, next) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        user = new User({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password 
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+      await user.save();
+      res.redirect('/login');
+    } else { 
+      res.status(400).send(`El usuario con email: ${req.body.email} ya existe.`)
+    };
+  } catch (e) {
+    next(e);
+  };
+});
+
+// Formulario de login
+app.get('/login',  async (req, res, next) => {
+  try {
+    res.render('login');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Inicio de sesion del usuario
+app.post('/login', async (req, res, next) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    req.session.userId = req.session.userId || user._id;
+    if (!user) {
+      res.status(300).send(`Usuario o email incorrecto`);
+    };
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if(!validPassword) {
+      res.status(300).send(`Usuario o email incorrecto`);
+    }    
+    res.redirect('/');
+  } catch (e) {
+    next(e);
+  };
+});
+
+// Cierre de sesion del usuario
+app.get('/logout', async (req, res) => {
+  req.session.userId = null;
+  res.redirect('/');
+})
 
 // formulario para crear una encuesta
 app.get("/polls/new", (req, res) => {
@@ -28,7 +98,7 @@ app.post("/polls", async (req, res, next) => {
     const data = {
       title: req.body.title,
       description: req.body.description,
-      user: '5d24f405f3c60aa3fad0e683',
+      user: '5d26c1e28960881724148913',
       options: [
         { text: req.body.option1 },
         { text: req.body.option2 },
--- models/User.js
+++ models/User.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  email: String,
-  name: String
+  email: { 
+    type: String,
+    required: true
+  }, 
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 const User = mongoose.model('User', userSchema);
